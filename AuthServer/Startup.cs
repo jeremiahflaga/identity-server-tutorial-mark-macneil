b0541c2e2e07c0630612df08c715fe4853e2dfa0
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2020-05-04 12:23:15 +0800

    init commit for Infrastructure project; fixed error " Duplicate 'System.Reflection.AssemblyCompanyAttribute' attribute"

--- AuthServer/Startup.cs
+++ AuthServer/Startup.cs
@@ -24,6 +24,26 @@ namespace AuthServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+      //      services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+      //      services.AddIdentity<AppUser, IdentityRole>()
+      //        .AddEntityFrameworkStores<AppIdentityDbContext>()
+      //        .AddDefaultTokenProviders();
+
+      //      services.AddIdentityServer().AddDeveloperSigningCredential()
+      //         // this adds the operational data from DB (codes, tokens, consents)
+      //         .AddOperationalStore(options =>
+      //         {
+      //             options.ConfigureDbContext = builder => builder.UseSqlServer(Configuration.GetConnectionString("Default"));
+      //    // this enables automatic token cleanup. this is optional.
+      //    options.EnableTokenCleanup = true;
+      //             options.TokenCleanupInterval = 30; // interval in seconds
+      //})
+      //         .AddInMemoryIdentityResources(Config.GetIdentityResources())
+      //         .AddInMemoryApiResources(Config.GetApiResources())
+      //         .AddInMemoryClients(Config.GetClients())
+      //         .AddAspNetIdentity<AppUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +72,8 @@ namespace AuthServer
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseIdentityServer();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# identity-server-tutorial-mark-macneil
-Code for the "User Authentication and Identity with Angular, Asp.Net Core and IdentityServer" tutorial by Mark Macneil
+# My code for the ["User Authentication and Identity with Angular, Asp.Net Core and IdentityServer"](https://fullstackmark.com/post/21/user-authentication-and-identity-with-angular-aspnet-core-and-identityserver) tutorial by Mark Macneil
+
